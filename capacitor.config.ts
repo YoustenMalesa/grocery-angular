fde59b3afa0a8d61c87a01b9812c728d358e8e24
Wildfly <youstenmalesa@gmail.com>
2023-05-01 12:24:46 +0200

    initial

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'za.co.tut.grocery',
+  appName: 'angular-ecommerce',
+  webDir: 'dist/angular-ecommerce',
+  bundledWebRuntime: false
+};
+
+export default config;
